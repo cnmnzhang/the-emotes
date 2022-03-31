9643a483fbaa05fcf0f4b85da0ab3b5a2b48c356
czhan117 <59889383+czhan117@users.noreply.github.com>
2022-03-31 19:47:47 -0400

    added homepage and some formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,9 @@
 import logo from './logo.svg';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl">The emotes</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomePage />
   );
 }
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl">The emotes</h1>
+      </header>
+    </div>
+  )
+}
+
+export default HomePage
